5a04ac89c8c446a3b744b938836100d18d7f1dee
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-04 02:50:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ def self.new_from_db(row)
     #data=DB[:conn].execute("SELECT * FROM dogs")
     binding.pry
 
-    self.new(id:row[0],name:row[1],creed:row[2])
+    self.new(id:row[0],name:row[1],breed:row[2])
     binding.pry
 end
 
